fd5f3d5cb583c512676c004072bdac4a70b414ac
swethabalusamy2007-commits <swethabalusamy2007@gmail.com>
2026-09-09 20:56:57 +0530

    Time: 15 ms (60.09%), Space: 51.6 MB (19.19%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0;
+        int j=0;
+        while(j<s.length&&i<g.length){
+            if(s[j]>=g[i]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
